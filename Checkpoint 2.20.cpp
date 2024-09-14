e96e342d2c12aee2e0867f3bffa0c209576ba8f9
N0sg <nathanaelseducation@gmail.com>
2024-09-14 01:37:06 -0400

    Add files via upload

---
+++ Checkpoint 2.20.cpp
@@ -0,0 +1,16 @@
+// Checkpoint 2.20.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Nathanael Goyette
+//9/13/24
+//IDE and VCS Setup Project
+//Checkpoint 2.20
+// I have two middle names so yeah 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age = 22;
+	double weight = 186;
+	cout << "My age is " << age << " and my weight is " << weight << " pounds.";
+	return 0;
+}
