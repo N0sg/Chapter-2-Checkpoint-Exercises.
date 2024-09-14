db0d62eec7337e6c2e6a44d73e6e64b8acc34087
N0sg <nathanaelseducation@gmail.com>
2024-09-14 01:36:43 -0400

    Add files via upload

---
+++ Checkpoint 2.17.cpp
@@ -0,0 +1,24 @@
+// Checkpoint 2.17.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//Nathanael Goyette
+//9/13/24
+//IDE and VCS Setup Project
+//Checkpoint 2.17
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    string address;
+    string phone;
+    name = "Nate";
+    address = "Im not having this that public on the internet";
+    phone = "988";
+    cout << "Name: " << name << endl;
+    cout << "Address: " << address << endl;
+    cout << "Phone Number: " << phone << endl;
+    return 0;
+}
