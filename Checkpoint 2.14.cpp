27e2c15e20e97fbc941a78a874980741c7e035bc
N0sg <nathanaelseducation@gmail.com>
2024-09-14 01:36:07 -0400

    Add files via upload

---
+++ Checkpoint 2.14.cpp
@@ -0,0 +1,20 @@
+// Checkpoint 2.14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Nathanael Goyette
+//9/13/24
+//IDE and VCS Setup Project
+//Checkpoint 2.14
+// I have two middle names so yeah 
+#include <iostream>
+#include <print>
+using namespace std;
+
+int main()
+{
+    char first, middle, middle2, last;
+    first = 'N';
+    middle = 'O';
+    middle2 = 'S';
+    last = 'G';
+    cout << first << " " << middle << "-" << middle2 << " " << last;
+
+}
